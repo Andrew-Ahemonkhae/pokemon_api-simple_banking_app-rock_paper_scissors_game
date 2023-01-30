5c78710dfac949a7ff34324eb994f9ccd082b858
Andrew <121820370+Andrew-Ahemonkhae@users.noreply.github.com>
2023-01-30 20:30:20 +0100

    Modified helloworld file multiple times while learning,  implemented pokemon API app and created new file for a simple class based python banking app.

---
+++ Banking_App.py
@@ -0,0 +1,12 @@
+# Banking app
+# class based
+# withdrawal and deposit
+# write the transaction to a python 
+# ==========================
+# while true
+# input
+# classes
+# method
+# properties
+# open()
+# deposit money, withdraw money, see transaction history in another file,   
--- Pokemon_API_application.py
+++ Pokemon_API_application.py
@@ -1,18 +1,14 @@
-#pokemon = input("question for the pokemon name")
 pokemon = input("What is your pokemon's name? ")
-print(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-# import requests
-# # print(f"https://{pokemon}.io")-this is the template for a dynamic url
-# req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-# req = req.json()#this helps change the json formatted API into a dictionary
-# print(req)
-#print(f"some textin here req[key]")- this will be me taking the key that holds a certain value about that pokemon and printing it out
+pokemon = pokemon.lower()
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+import requests
 
+req = requests.get(url)
 
+req = req.json()
 
-
-# ask for user input: ask for a pokemon
-# create a dynamic url that requests api data for that pokemon
-# fetch the data for that url
-# print out the json data as a dictionary
-# print out pokemon data based on what is available in the api data
+print(type(req))
+print(f"The pokemon's name is {req['name']}")
+print("The pokemon's abilities are :")
+for ability in req['abilities']:
+    print(ability['ability'] ['name'])
